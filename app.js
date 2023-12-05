0e524dca283103208897a61e792689b0d1cbc8a2
Chinmay <chinz.sandur07@gmail.com>
2023-12-05 15:27:26 +0530

    add output

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const selectionButtons = document.querySelectorAll('[data-selection]');
+const finalColumn = document.querySelector("[data-final-column]");
+const computerScoreSpan = document.querySelector("[data-computer-score]")
+const yourScoreSpan = document.querySelector("[data-your-score]");
+
 const SELECTIONS = [
     {
         name: 'rock',
@@ -27,13 +31,23 @@ selectionButtons.forEach(selectionButton => {
 })
 
 function makeSelection(selection) {
-    const computerSelection  = randomSelection();
+    const computerSelection = randomSelection();
     const yourwinner = isWinner(selection, computerSelection);
     const computerWinner = isWinner(computerSelection, selection);
+    addSelectionResult(computerSelection, computerWinner);
+    addSelectionResult(selection, yourwinner);
     console.log(computerSelection);
 }
 
-function isWinner(selection, opponentSelection){
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji;
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div);
+}
+
+function isWinner(selection, opponentSelection) {
     return selection.beats === opponentSelection.name
 }
 
